a03d1d36bb1573b6d339329017510dfea14ebd11
Daniel Son <dson.email@gmail.com>
2022-03-06 18:47:57 +1100

    TS Classes - Play with access modifiers

--- public/app.js
+++ public/app.js
@@ -1,19 +1,42 @@
 "use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
+var Invoice = /** @class */ (function () {
+    /* private client: string;
+    details: string;
+    readonly amount: number;
+  
+    constructor(c: string, d: string, a: number) {
+      this.client = c;
+      this.details = d;
+      this.amount = a;
+    } */
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    ;
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes $").concat(this.amount, " for ").concat(details);
+    };
+    return Invoice;
+}());
+var invoiceOne = new Invoice("Mario", "work on website", 350);
+var invoiceTwo = new Invoice("Luigi", "work on website", 200);
+var invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+//console.log(invoiceOne.client);
+console.log(invoiceOne.details);
+//invoiceOne.amount = 500;
 //const form = document.querySelector('form')!;
-var form = document.querySelector('.new-item-form');
+var form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("Dannnnn");
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,48 @@
-const anchor = document.querySelector('a')!;
-if(anchor) {
-  console.log(anchor.href);
+class Invoice {
+  /* private client: string;
+  details: string;
+  readonly amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  } */
+
+  constructor(
+    private client: string,
+    public details: string,
+    readonly amount: number
+  ){};
+  
+  format() {
+    return `${this.client} owes $${this.amount} for ${details}`;
+  }
 }
-console.log(anchor.href);
+
+const invoiceOne = new Invoice("Mario", "work on website", 350);
+const invoiceTwo = new Invoice("Luigi", "work on website", 200);
+
+const invoices: Invoice[] = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+
+//console.log(invoiceOne.client);
+console.log(invoiceOne.details);
+//invoiceOne.amount = 500;
 
 //const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
+form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log("Dannnnn");
-
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
